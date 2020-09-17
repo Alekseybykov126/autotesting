fd1f40ba7ef12cd946f31dce341e923da287700c
aleh126 <70600596+Alekseybykov126@users.noreply.github.com>
2020-09-17 22:54:04 +0300

    Update page.py

--- page.py
+++ page.py
@@ -5,7 +5,7 @@ from selenium.webdriver.common.action_chains import ActionChains
 from selenium import webdriver
 #from bs4 import BeautifulSoup
 from selenium import webdriver
-from selenium.webdriver.firefox.webdriver import Webdriver
+#from selenium.webdriver.firefox.webdriver import Webdriver
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.keys import Keys
